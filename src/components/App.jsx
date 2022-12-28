8286dc3789be045f7332c109644139248e95ff41
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2022-12-28 21:48:03 +0200

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import { Profile } from './Profile/Profile';
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101'
+      // }}
     >
-      React homework template
+      <Profile 
+      username  //ім'я користувача
+      tag       //тег в соціальній мережі
+      location  //місто і країна
+      avatar    //посилання на зображення
+      stats     // об'єкт з інформацією про активності
+      />
     </div>
   );
 };
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,9 @@
+
+
+export function Profile({
+    username,
+    tag,
+    location,
+    avatar,
+    stats: { followers, views, likes },
+  }) {}
